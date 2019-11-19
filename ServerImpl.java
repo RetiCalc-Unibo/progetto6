15681aea56cf30cba61c3e2104948bba18ada896
IvanAndreiDaniel <56355504+IvanAndreiDaniel@users.noreply.github.com>
2019-11-19 22:45:31 +0100

    Ordinamento testo (ma non ho usato il sort Xd)

--- ServerImpl.java
+++ ServerImpl.java
@@ -5,30 +5,29 @@ import java.util.StringTokenizer;
 
 public class ServerImpl implements RemOp {
 
-
     @Override
     public int conta_righe(String fileName, int threshold, String delim) throws RemoteException {
         if (!fileName.endsWith(".txt")) {
-            throw new RemoteException(fileName + " isn't a text file");
+            throw new RemoteException(fileName + " is not a text file");
         }
+        
         File file = new File(fileName);
-        /*if (!file.isFile()) {
-            throw new RemoteException(fileName + " doesn't exist");
-        }*/
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
             int occ = 0;
+
             while ((line = br.readLine()) != null) {
                 StringTokenizer tk = new StringTokenizer(line, delim);
                 if (tk.countTokens() > threshold) {
                     occ++;
                 }
             }
+
             return occ;
         } catch (FileNotFoundException e) {
-            throw new RemoteException(fileName + " doesn't exist");
+            throw new RemoteException(fileName + " does not exist");
         } catch (IOException e) {
             throw new RemoteException("Unexpected read error!");
         }
@@ -37,28 +36,23 @@ public class ServerImpl implements RemOp {
     @Override
     public int elimina_riga(String fileName, int rowNum) throws RemoteException {
         return 0;
-
     }
-
 
     // Avvio del Server RMI
     public static void main(String[] args) {
-
         final int REGISTRYPORT = 1099;
         String registryHost = "localhost";
-        String serviceName = "Server";        //lookup name...
+        String serviceName = "Server";
 
         // Registrazione del servizio RMI
-        String completeName = "//" + registryHost + ":" + REGISTRYPORT + "/"
-                + serviceName;
+        String completeName = "//" + registryHost + ":" + REGISTRYPORT + "/" + serviceName;
+
         try {
             ServerImpl serverRMI = new ServerImpl();
             Naming.rebind(completeName, serverRMI);
-            System.out.println("Server RMI: Servizio \"" + serviceName
-                    + "\" registrato");
+            System.out.println("Server RMI: Servizio \"" + serviceName + "\" registrato");
         } catch (Exception e) {
-            System.err.println("Server RMI \"" + serviceName + "\": "
-                    + e.getMessage());
+            System.err.println("Server RMI \"" + serviceName + "\": " + e.getMessage());
             e.printStackTrace();
             System.exit(1);
         }
