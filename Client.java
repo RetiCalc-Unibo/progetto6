92983ca0d4cb3958e5b165de50d69dc5554d7909
KatChup <karina.chichifoi@gmail.com>
2019-11-20 09:22:00 +0100

    Ho problemi con Github
    Sono autistica

--- Client.java
+++ Client.java
@@ -1,15 +1,15 @@
 import java.io.*;
 import java.rmi.*;
 
-public class Client{
-	public static void main(String[] args){
+public class Client {
+	public static void main(String[] args) {
 		final int REGISTRYPORT = 1099;
 		String registryHost = null;
 
 		String serviceName = "";
 		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-		if(args.length != 2){
+		if (args.length != 2) {
 			System.out.println("[C] Syntax: java Client RegistryHost [RegistryPort]");
 			System.exit(1);
 		}
@@ -29,52 +29,58 @@ public class Client{
 
 			System.out.print("[C] Servizio (C = Conta Parole - E = Elimina Riga): ");
 
-			while((service = stdIn.readLine()) != null){
-				if(service.equals("C")){
-					 String fileName, delim;
-					 int numParole;
+			while ((service = stdIn.readLine()) != null) {
+				if (service.equals("C")) {
+					String fileName, delim;
+					int numParole;
 
-					 // Ricevo in input il nome del file
-					 System.out.print("[C] Inserisci il nome del file: ");
-					 fileName = stdIn.readLine();
-					 // Ricevo l'intero
-					 System.out.print("[C] Inserisci il numero di parole per riga da ricercare: ");
-					 numParole = Integer.parseInt(stdIn.readLine());
-					 // Ricevo i separatori
-					 System.out.print("[C] Inserisci i separatori: ");
-					 delim = stdIn.readLine();
+					// Ricevo in input il nome del file
+					System.out.print("[C] Inserisci il nome del file: ");
+					fileName = stdIn.readLine();
+					// Ricevo l'intero
+					System.out.print("[C] Inserisci il numero di parole per riga da ricercare: ");
+					numParole = Integer.parseInt(stdIn.readLine());
+					// Ricevo i separatori
+					System.out.print("[C] Inserisci i separatori: ");
+					delim = stdIn.readLine();
 
-					 if(serverRMI.conta_righe(fileName, numParole, delim) == 0)
-					 	System.out.println("[C] Conto le righe del file " + fileName + " con uno threshold di " + numParole + " e come delimitatori " + delim);
-					 else
-					 	System.out.println("[C] Servizio attualmente non disponibile");
-				} else if(service.equals("E")){
-					 String fileName;
-					 int numRiga;
+					if (serverRMI.conta_righe(fileName, numParole, delim) == 0) {
+						System.out.println("[C] Conto le righe del file " + fileName + " con uno threshold di " + numParole + " e come delimitatori " + delim);
+					}
+					else {
+						System.out.println("[C] Servizio attualmente non disponibile");
+					}
+				}
+				else if (service.equals("E")) {
+					String fileName;
+					int numRiga;
 
-					 // Ricevo in input il nome del file
-					 System.out.print("[C] Inserisci il nome del file: ");
-					 fileName = stdIn.readLine();
-					 // Ricevo l'intero
-					 System.out.print("[C] Inserisci il la riga da eliminare: ");
-					 numRiga = Integer.parseInt(stdIn.readLine());
+					// Ricevo in input il nome del file
+					System.out.print("[C] Inserisci il nome del file: ");
+					fileName = stdIn.readLine();
+					// Ricevo l'intero
+					System.out.print("[C] Inserisci il la riga da eliminare: ");
+					numRiga = Integer.parseInt(stdIn.readLine());
 
-					 if(serverRMI.elimina_riga(fileName, numRiga) == 0)
-					 	System.out.println("[C] Elimino la riga " + numRiga + " dal file " + fileName);
-					 else
-					 	System.out.println("[C] Servizio attualmente non disponibile");
-				} else {
+					if (serverRMI.elimina_riga(fileName, numRiga) == 0) {
+						System.out.println("[C] Elimino la riga " + numRiga + " dal file " + fileName);
+					}
+					else {
+						System.out.println("[C] Servizio attualmente non disponibile");
+					}
+				}
+				else {
 					System.out.println("[C] Servizio non esistente!");
 					System.out.println("[C] Servizi disponibili: C -> Conta Parole; E -> Elimina Riga");
 				}
 			}
-		} catch(RemoteException re){
+		} catch(RemoteException re) {
 			System.err.println("[C] Possibile errore nel file; " + re.getMessage());
 			re.printStackTrace();
-		} catch(NotBoundException nbe){
+		} catch(NotBoundException nbe) {
 			System.err.println("[C] Il nome fornito non risulta nel resgistry; " + nbe.getMessage());
 			System.exit(2);
-		} catch(Exception e){
+		} catch(Exception e) {
 			System.err.println("[C] " + e.getMessage());
 			e.printStackTrace();
 			System.exit(1);
--- RemOp.java
+++ RemOp.java
@@ -1,17 +1,10 @@
-/**
- * Interfaccia remota di servizio
- */
+// Interfaccia remota di servizio
 
 import java.io.FileNotFoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RemOp extends Remote {
-
-    public int conta_righe(String fileName, int threshold, String delim)
-            throws RemoteException, FileNotFoundException;
-
+    public int conta_righe(String fileName, int threshold, String delim) throws RemoteException, FileNotFoundException;
     public int elimina_riga(String fileName, int rowNum) throws RemoteException;
-
 }
-
